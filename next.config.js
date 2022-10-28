34e8cf044dd68c242352dff305f1f154cd4abdc1
giangit112000 <giangnt.it0101@gmail.com>
2022-10-28 08:52:42 +0700

    init food_bo

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-        HOST_NAME_API: process.env.HOST_NAME_API,
-    },
-    images: {
-        domains: [process.env.DOMAIN_NAME_API],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    HOST_NAME_API: process.env.HOST_NAME_API,
+  },
 };
 
 module.exports = nextConfig;
